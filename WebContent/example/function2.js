1e42a7d78367340218415efe5b9e04ca344a9fe6
leadon <leadon@empal.com>
2020-03-31 13:32:51 +0900

    a

---
+++ WebContent/example/function2.js
@@ -0,0 +1,27 @@
+var score = 10;
+function myFunc() {
+    var score = 20;
+    // console.log("local: " + score);
+}
+myFunc();
+// console.log("global: " + score);
+
+function newFunc() {
+    // console.log("global function");
+}
+
+function outerFunc() {
+    function newFunc() {
+        // console.log("local function");
+    }
+    newFunc();
+}
+
+newFunc();
+outerFunc();
+
+(function() {
+    // console.log("return Fnc");
+})();
+
+
